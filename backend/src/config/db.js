4e12081282612b693fba1dfb64fd774df6141890
DragzterX <siamsaimum@gmail.com>
2025-09-09 05:55:15 +0600

    Add new files and update existing ones Backend done for now

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const env = require('./env'); // ✅ make sure you import env.js
+const env = require('./env'); // make sure you import env.js
 
 const connect = async () => {
   try {
